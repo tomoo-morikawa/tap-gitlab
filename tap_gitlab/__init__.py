52adf91784e5dd3ff391ac8594e732cd81d22881
tomoo.morikawa <tomoo.morikawa.84@shiftinc.jp>
2023-01-06 09:57:21 +0900

    enabled to tap merge_request api

--- tap_gitlab/__init__.py
+++ tap_gitlab/__init__.py
@@ -13,59 +13,67 @@ from strict_rfc3339 import rfc3339_to_timestamp
 
 PER_PAGE = 100
 CONFIG = {
-    'api_url': "https://gitlab.com/api/v3",
-    'private_token': None,
-    'start_date': None,
-    'groups': ''
+    "api_url": "https://gitlab.com/api/v3",
+    "private_token": None,
+    "start_date": None,
+    "groups": "",
 }
 STATE = {}
+
 
 def get_abs_path(path):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
 
+
 def load_schema(entity):
     return utils.load_json(get_abs_path("schemas/{}.json".format(entity)))
 
+
 RESOURCES = {
-    'projects': {
-        'url': '/projects/{}',
-        'schema': load_schema('projects'),
-        'key_properties': ['id'],
+    "projects": {
+        "url": "/projects/{}",
+        "schema": load_schema("projects"),
+        "key_properties": ["id"],
     },
-    'branches': {
-        'url': '/projects/{}/repository/branches',
-        'schema': load_schema('branches'),
-        'key_properties': ['project_id', 'name'],
+    "branches": {
+        "url": "/projects/{}/repository/branches",
+        "schema": load_schema("branches"),
+        "key_properties": ["project_id", "name"],
     },
-    'commits': {
-        'url': '/projects/{}/repository/commits',
-        'schema': load_schema('commits'),
-        'key_properties': ['id'],
+    "commits": {
+        "url": "/projects/{}/repository/commits",
+        "schema": load_schema("commits"),
+        "key_properties": ["id"],
     },
-    'issues': {
-        'url': '/projects/{}/issues',
-        'schema': load_schema('issues'),
-        'key_properties': ['id'],
+    "issues": {
+        "url": "/projects/{}/issues",
+        "schema": load_schema("issues"),
+        "key_properties": ["id"],
     },
-    'project_milestones': {
-        'url': '/projects/{}/milestones',
-        'schema': load_schema('milestones'),
-        'key_properties': ['id'],
+    "project_milestones": {
+        "url": "/projects/{}/milestones",
+        "schema": load_schema("milestones"),
+        "key_properties": ["id"],
     },
-    'group_milestones': {
-        'url': '/groups/{}/milestones',
-        'schema': load_schema('milestones'),
-        'key_properties': ['id'],
+    "group_milestones": {
+        "url": "/groups/{}/milestones",
+        "schema": load_schema("milestones"),
+        "key_properties": ["id"],
     },
-    'users': {
-        'url': '/projects/{}/users',
-        'schema': load_schema('users'),
-        'key_properties': ['id'],
+    "users": {
+        "url": "/projects/{}/users",
+        "schema": load_schema("users"),
+        "key_properties": ["id"],
     },
-    'groups': {
-        'url': '/groups/{}',
-        'schema': load_schema('groups'),
-        'key_properties': ['id'],
+    "groups": {
+        "url": "/groups/{}",
+        "schema": load_schema("groups"),
+        "key_properties": ["id"],
+    },
+    "merge_requests": {
+        "url": "/projects/{}/merge_requests",
+        "schema": load_schema("merge_requests"),
+        "key_properties": ["id"],
     },
 }
 
@@ -78,93 +86,102 @@ def get_url(entity, id):
     if not isinstance(id, int):
         id = id.replace("/", "%2F")
 
-    return CONFIG['api_url'] + RESOURCES[entity]['url'].format(id)
+    return CONFIG["api_url"] + RESOURCES[entity]["url"].format(id)
 
 
 def get_start(entity):
     if entity not in STATE:
-        STATE[entity] = CONFIG['start_date']
+        STATE[entity] = CONFIG["start_date"]
 
     return STATE[entity]
 
 
-@backoff.on_exception(backoff.expo,
-                      (requests.exceptions.RequestException),
-                      max_tries=5,
-                      giveup=lambda e: e.response is not None and 400 <= e.response.status_code < 500, # pylint: disable=line-too-long
-                      factor=2)
+@backoff.on_exception(
+    backoff.expo,
+    (requests.exceptions.RequestException),
+    max_tries=5,
+    giveup=lambda e: e.response is not None
+    and 400 <= e.response.status_code < 500,  # pylint: disable=line-too-long
+    factor=2,
+)
 def request(url, params=None):
     params = params or {}
-    params['private_token'] = CONFIG['private_token']
+    params["private_token"] = CONFIG["private_token"]
 
     headers = {}
-    if 'user_agent' in CONFIG:
-        headers['User-Agent'] = CONFIG['user_agent']
+    if "user_agent" in CONFIG:
+        headers["User-Agent"] = CONFIG["user_agent"]
 
-    req = requests.Request('GET', url, params=params, headers=headers).prepare()
+    req = requests.Request("GET", url, params=params, headers=headers).prepare()
     LOGGER.info("GET {}".format(req.url))
     resp = SESSION.send(req)
 
     if resp.status_code >= 400:
         LOGGER.critical(
             "Error making request to GitLab API: GET {} [{} - {}]".format(
-                req.url, resp.status_code, resp.content))
+                req.url, resp.status_code, resp.content
+            )
+        )
         sys.exit(1)
 
     return resp
 
 
 def gen_request(url):
-    params = {'page': 1}
+    params = {"page": 1}
     resp = request(url, params)
-    last_page = int(resp.headers.get('X-Total-Pages', 1))
+    last_page = int(resp.headers.get("X-Total-Pages", 1))
 
     for row in resp.json():
         yield row
 
     for page in range(2, last_page + 1):
-        params['page'] = page
+        params["page"] = page
         resp = request(url, params)
         for row in resp.json():
             yield row
 
+
 def format_timestamp(data, typ, schema):
     result = data
-    if typ == 'string' and schema.get('format') == 'date-time':
+    if typ == "string" and schema.get("format") == "date-time" and data is not None:
         rfc3339_ts = rfc3339_to_timestamp(data)
         utc_dt = datetime.datetime.utcfromtimestamp(rfc3339_ts).replace(tzinfo=pytz.UTC)
         result = utils.strftime(utc_dt)
 
     return result
 
+
 def flatten_id(item, target):
     if target in item and item[target] is not None:
-        item[target + '_id'] = item.pop(target, {}).pop('id', None)
+        item[target + "_id"] = item.pop(target, {}).pop("id", None)
     else:
-        item[target + '_id'] = None
+        item[target + "_id"] = None
 
 
 def sync_branches(project):
-    url = get_url("branches", project['id'])
+    url = get_url("branches", project["id"])
     with Transformer(pre_hook=format_timestamp) as transformer:
         for row in gen_request(url):
-            row['project_id'] = project['id']
+            row["project_id"] = project["id"]
             flatten_id(row, "commit")
-            transformed_row = transformer.transform(row, RESOURCES["branches"]["schema"])
+            transformed_row = transformer.transform(
+                row, RESOURCES["branches"]["schema"]
+            )
             singer.write_record("branches", transformed_row, time_extracted=utils.now())
 
 
 def sync_commits(project):
-    url = get_url("commits", project['id'])
+    url = get_url("commits", project["id"])
     with Transformer(pre_hook=format_timestamp) as transformer:
         for row in gen_request(url):
-            row['project_id'] = project["id"]
+            row["project_id"] = project["id"]
             transformed_row = transformer.transform(row, RESOURCES["commits"]["schema"])
             singer.write_record("commits", transformed_row, time_extracted=utils.now())
 
 
 def sync_issues(project):
-    url = get_url("issues", project['id'])
+    url = get_url("issues", project["id"])
     with Transformer(pre_hook=format_timestamp) as transformer:
         for row in gen_request(url):
             flatten_id(row, "author")
@@ -173,21 +190,28 @@ def sync_issues(project):
             transformed_row = transformer.transform(row, RESOURCES["issues"]["schema"])
 
             if row["updated_at"] >= get_start("project_{}".format(project["id"])):
-                singer.write_record("issues", transformed_row, time_extracted=utils.now())
+                singer.write_record(
+                    "issues", transformed_row, time_extracted=utils.now()
+                )
 
 
 def sync_milestones(entity, element="project"):
-    url = get_url(element + "_milestones", entity['id'])
+    url = get_url(element + "_milestones", entity["id"])
 
     with Transformer(pre_hook=format_timestamp) as transformer:
         for row in gen_request(url):
-            transformed_row = transformer.transform(row, RESOURCES[element + "_milestones"]["schema"])
+            transformed_row = transformer.transform(
+                row, RESOURCES[element + "_milestones"]["schema"]
+            )
 
             if row["updated_at"] >= get_start(element + "_{}".format(entity["id"])):
-                singer.write_record(element + "_milestones", transformed_row, time_extracted=utils.now())
+                singer.write_record(
+                    element + "_milestones", transformed_row, time_extracted=utils.now()
+                )
+
 
 def sync_users(project):
-    url = get_url("users", project['id'])
+    url = get_url("users", project["id"])
     project["users"] = []
     with Transformer(pre_hook=format_timestamp) as transformer:
         for row in gen_request(url):
@@ -197,7 +221,7 @@ def sync_users(project):
 
 
 def sync_group(gid, pids):
-    url = CONFIG['api_url'] + RESOURCES["groups"]['url'].format(gid)
+    url = CONFIG["api_url"] + RESOURCES["groups"]["url"].format(gid)
 
     data = request(url).json()
     time_extracted = utils.now()
@@ -207,9 +231,9 @@ def sync_group(gid, pids):
 
     if not pids:
         #  Get all the projects of the group if none are provided
-        for project in group['projects']:
-            if project['id']:
-                pids.append(project['id'])
+        for project in group["projects"]:
+            if project["id"]:
+                pids.append(project["id"])
 
     for pid in pids:
         sync_project(pid)
@@ -217,6 +241,25 @@ def sync_group(gid, pids):
     sync_milestones(group, "group")
 
     singer.write_record("groups", group, time_extracted=time_extracted)
+
+
+def sync_merge_request(project):
+    url = get_url("merge_requests", project["id"])
+    with Transformer(pre_hook=format_timestamp) as transformer:
+        for row in gen_request(url):
+            flatten_id(row, "merged_by")
+            flatten_id(row, "closed_by")
+            flatten_id(row, "merge_user")
+            flatten_id(row, "assignee")
+            flatten_id(row, "author")
+            transformed_row = transformer.transform(
+                row, RESOURCES["merge_requests"]["schema"]
+            )
+
+            if row["updated_at"] >= get_start("project_{}".format(project["id"])):
+                singer.write_record(
+                    "merge_requests", transformed_row, time_extracted=utils.now()
+                )
 
 
 def sync_project(pid):
@@ -230,22 +273,24 @@ def sync_project(pid):
 
     state_key = "project_{}".format(project["id"])
 
-    #pylint: disable=maybe-no-member
-    last_activity_at = project.get('last_activity_at', project.get('created_at'))
+    # pylint: disable=maybe-no-member
+    last_activity_at = project.get("last_activity_at", project.get("created_at"))
     if not last_activity_at:
         raise Exception(
-            #pylint: disable=line-too-long
-            "There is no last_activity_at or created_at field on project {}. This usually means I don't have access to the project."
-            .format(project['id']))
+            # pylint: disable=line-too-long
+            "There is no last_activity_at or created_at field on project {}. This usually means I don't have access to the project.".format(
+                project["id"]
+            )
+        )
 
-
-    if project['last_activity_at'] >= get_start(state_key):
+    if project["last_activity_at"] >= get_start(state_key):
 
         sync_branches(project)
         sync_commits(project)
         sync_issues(project)
         sync_milestones(project)
         sync_users(project)
+        sync_merge_request(project)
 
         singer.write_record("projects", project, time_extracted=time_extracted)
         utils.update_state(STATE, state_key, last_activity_at)
@@ -255,11 +300,11 @@ def sync_project(pid):
 def do_sync():
     LOGGER.info("Starting sync")
 
-    gids = list(filter(None, CONFIG['groups'].split(' ')))
-    pids = list(filter(None, CONFIG['projects'].split(' ')))
+    gids = list(filter(None, CONFIG["groups"].split(" ")))
+    pids = list(filter(None, CONFIG["projects"].split(" ")))
 
     for resource, config in RESOURCES.items():
-        singer.write_schema(resource, config['schema'], config['key_properties'])
+        singer.write_schema(resource, config["schema"], config["key_properties"])
 
     for gid in gids:
         sync_group(gid, pids)
@@ -292,5 +337,5 @@ def main():
         raise exc
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
